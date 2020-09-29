7fb214404bfb01cab5a22b53f9b64bf9f2bfff10
OakBricks <65257189+OakBricks@users.noreply.github.com>
2020-09-29 16:38:06 -0500

    Update update-amoebacore.sh

--- update-amoebacore.sh
+++ update-amoebacore.sh
@@ -1,2 +1,8 @@
-tce-load -wi git dialog
+tce-load -wi wget dialog
 echo "This script will update Amoeba Linux"
+wget https://raw.githubusercontent.com/AmoebaLinux/AmoebaLinuxUpdates/master/update-amoebacore.sh > /tmp/pkglist
+tce-load -wi $(echo /tmp/pkglist)
+wget https://raw.githubusercontent.com/AmoebaLinux/AmoebaLinuxUpdates/master/update-amoebacore.sh > /tmp/delpkglist
+tce-remove $(echo /tmp/delpkglist)
+wget https://raw.githubusercontent.com/AmoebaLinux/AmoebaLinuxUpdates/master/filelistdelete-amoebacore > /tmp/delfilelist
+rm -rf $(echo /tmp/delfilelist)
